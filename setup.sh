940763d47fa7f882aa8de245ad32c2931bb08176
IakovosTzoka <iakovos.tzoka@uta.edu>
2026-04-10 16:59:11 -0400

    Fix build for Apple Silicon MacOs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-export CC=/usr/bin/gcc-12
-export CXX=/usr/bin/g++-12
-
-
+export CC=/opt/homebrew/bin/gcc-12
+export CXX=/opt/homebrew/bin/g++-12
+export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+export LIBRARY_PATH="$LIBRARY_PATH:$SDKROOT/usr/lib"
+export CFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX14.sdk"
+export CXXFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX14.sdk"
+export CXXFLAGS="$CXXFLAGS -I/opt/homebrew/opt/nlohmann-json/include"
+       
 
